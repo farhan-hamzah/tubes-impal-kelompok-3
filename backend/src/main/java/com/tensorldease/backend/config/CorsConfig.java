251c080d667525287273a2d9843065d8da8e4c01
Farhan Hamzah <farha-hamzah@users.noreply.github.com>
2026-05-01 14:58:08 +0700

    semoga bisa di deploy

--- backend/src/main/java/com/tensorldease/backend/config/CorsConfig.java
+++ backend/src/main/java/com/tensorldease/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:5173"
+            "*"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
