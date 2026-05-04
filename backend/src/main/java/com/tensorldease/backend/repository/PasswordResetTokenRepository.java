e81ee06d1edcbd5bc2b6e53e94fd8f07f24b2167
Farhan Hamzah <farha-hamzah@users.noreply.github.com>
2026-05-04 09:23:56 +0700

    04-05-2026

--- backend/src/main/java/com/tensorldease/backend/repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/tensorldease/backend/repository/PasswordResetTokenRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
     Optional<PasswordResetToken> findByToken(String token);
+    @Transactional
     void deleteByUserUserId(String userId);
 }
