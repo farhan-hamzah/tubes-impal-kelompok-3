fb4dfbdac5ab73b7379b9556e2cfdf7b1587d344
Farhan Hamzah <farha-hamzah@users.noreply.github.com>
2026-05-04 09:02:45 +0700

    04-05-2026

--- backend/src/main/java/com/tensorldease/backend/config/CorsConfig.java
+++ backend/src/main/java/com/tensorldease/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
+        config.setAllowedOriginPatterns(List.of(
             "*"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
