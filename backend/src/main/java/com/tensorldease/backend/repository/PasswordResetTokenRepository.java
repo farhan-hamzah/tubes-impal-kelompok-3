90e1e71eeef3288570dadac7dfcb874194ec2cce
Farhan Hamzah <farha-hamzah@users.noreply.github.com>
2026-05-09 05:40:46 +0700

    Revert "04-05-2026"
    This reverts commit 1fc9c2dc96488a4b1efb598e8d946fe8fd794afd.

--- backend/src/main/java/com/tensorldease/backend/repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/tensorldease/backend/repository/PasswordResetTokenRepository.java
@@ -4,7 +4,6 @@ import com.tensorldease.backend.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
